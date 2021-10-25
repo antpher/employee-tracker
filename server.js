e6f992758a93b7077ea1ba67d809507b3d7662d7
Anthony Her <antpher@gmail.com>
2021-10-24 20:50:17 -0400

    final

--- server.js
+++ server.js
@@ -55,3 +55,110 @@ const viewAllEmployees = () => {
         console.table(results);
         promptUser();
     })
+}
+
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Which department do you want to add?'
+        }
+
+    ]).then(function (data) {
+        db.query('INSERT INTO department SET ?', data, function (err, results) {
+            if (err) throw err
+            console.log('Department added');
+            promptUser();
+        })
+    })
+}
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Which role do you want to add?'
+        },
+        {
+            type: 'input',
+            name: 'department_id',
+            message: 'Which department?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary?'
+        }
+
+    ]).then(function (data) {
+        db.query('INSERT INTO role SET ?', data, function (err, results) {
+            if (err) throw err
+            console.log('Role added');
+            promptUser();
+        })
+    })
+}
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: 'What is the first name?'
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: 'What is the last name?'
+        },
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role id?'
+        },
+        {
+            type: 'input',
+            name: 'manager_id',
+            message: 'What is the role id?'
+        }
+
+    ]).then(function (data) {
+        db.query('INSERT INTO employee SET ?', data, function (err, results) {
+            if (err) throw err
+            console.log('Employee added');
+            promptUser();
+        })
+    })
+}
+
+const updateEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'role_id',
+            message: 'What is the role id?'
+        },
+        {
+            type: 'input',
+            name: 'employee_id',
+            message: 'What is the employee id?'
+        }
+
+    ]).then(function (data) {
+        db.query('UPDATE employee SET role_id = ? where id = ?', [data.role_id, data.employee_id], function (err, results) {
+            if (err) throw err
+            console.log('Employee updated');
+            promptUser();
+        })
+    })
+}
+
+function init() {
+    promptUser();
+}
+db.connect(function (err) {
+    if (err) throw err
+    init();
+})
